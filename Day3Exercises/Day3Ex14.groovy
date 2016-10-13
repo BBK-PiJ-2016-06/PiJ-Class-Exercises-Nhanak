4156a84b74e70beb6d84173d3163710c2addacb8
Nathan Hanak <nathanhanak@NathanHsMacBook.home>
2016-10-13 01:01:28 +0100

    forgot Day3Ex14

--- Day3Exercises/Day3Ex14.groovy
+++ Day3Exercises/Day3Ex14.groovy
@@ -1,24 +1,13 @@
-println "Please enter a word or a phrase: "
-String str = System.console().readLine()
-boolean isPalindrome = true
-int stringLength = str.length()
-int stringCrawler = 0
-int spacesFromBack = str.length()
-char frontToBack, backToFront
+println "please enter some text"
+String input = System.console().readLine()
+int inputLength = input.length()
+boolean finished = false
+String palindrome = ""
+String letterAdd = ""
 
-for (stringCrawler = 0; stringCrawler < stringLength; stringCrawler++) {
-  spacesFromBack = spacesFromBack - 1
-  frontToBack = str.charAt(stringCrawler)
-  backToFront = str.charAt(spacesFromBack)
-  if (frontToBack != backToFront) {
-    isPalindrome = false
-    break;
-  }
-}
+for (int stringCrawl = 0; stringCrawl < inputLength; stringCrawl++) {
+    letterAdd = input.charAt(stringCrawl)
+    palindrome = letterAdd + palindrome
+    }
 
-
-if (isPalindrome) {
-  println "What you wrote is a dang strict palindrome."
-} else {
-  println "What you wrote ain't a dang strict palindrome"
-  }
+println input + palindrome

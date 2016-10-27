4b3da02757463b08331b795740d6baecf85ccc72
Nathan Hanak <nathanhanak@Nathan-Hanaks-MacBook.local>
2016-10-27 12:37:29 +0100

    fixed Day4 Ex 03 bug of it adding 1 to binary to decimal results, truncated code

--- Day4Exercises/Day4Ex03.groovy
+++ Day4Exercises/Day4Ex03.groovy
@@ -1,9 +1,6 @@
 int power(int b, int e) {
-  int counter = 0
-  result = b
-  for (counter = 1; counter<e; counter++) {
-    result = result * b
-  }
+  int result = 0;
+  result = Math.pow(b,e)
   return result;
 } // once you leave here b and e are wiped out. result is remembered though
 
@@ -12,20 +9,20 @@ int power2(int exponent) {
   return this.result
 }
 /// reads the binary number entered, sends each digit one by one to get converted in to a decimal
-String binary2decimal(String biString) {
-  int biNumLength = biString.length()
-  int total = 0
-  int exponentSend = biNumLength - 1
-  int biConvert = 0
+int binary2decimal(String biString) {
+  int exponentSend = biString.length() - 1
   int resultInt = 0
-  for (int stringCrawler = 0; stringCrawler < biNumLength; stringCrawler++) {
-    String singleDigit = biString.charAt(stringCrawler)
-    biConvert = Integer.parseInt(singleDigit)
-    resultInt = resultInt + (biConvert * power2(exponentSend))  // this part adds each digit up in to a decimal
-    exponentSend = exponentSend - 1
+  for (int stringCrawler = 0; stringCrawler < biString.length(); stringCrawler++) {
+    println "one by one the chars are: " + biString.charAt(stringCrawler)
+    if (biString.charAt(stringCrawler) == "1") {
+    resultInt = resultInt + power2(exponentSend)  // this part adds each digit up in to a decimal
+    exponentSend--
+    println "my result so far is " + resultInt
+    } else {
+        exponentSend--
+      }
   }
-  binaryResult = resultInt
-  return binaryResult
+  return resultInt
 }
 /// turns decimal number in to a binary number
 String decimal2binary(int decimal) {
@@ -61,8 +58,7 @@ while (running) {
     case 1: println "Please enter your binary number";
       print ">>>";
       binaryInput = System.console().readLine();
-      binary2decimal(binaryInput);
-      println "That binary number equals: " + binaryResult;
+      println "That binary number equals: " + binary2decimal(binaryInput);
       break;
     case 2: println "Please enter your base 10 number";
       print ">>>";

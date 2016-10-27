0a2f89068a1f4dd3c766aef4bd545896944e17b7
Nathan Hanak <nathanhanak@NathanHsMacBook.home>
2016-10-27 20:59:05 +0100

    Loading completed Day8Ex03 exercises to a git folder. MobilePhone contact list still a little buggy, not storing numbers properly beyond 10

---
+++ Day8Exercises/Day08Ex03/MobilePhone.java
@@ -0,0 +1,59 @@
+public class MobilePhone extends OldPhone {
+    private String[] callHistory; // will store the last ten numbers called
+
+    public void ringAlarm(String alarm) { // sets off an alarm
+          System.out.println("BUZZ BUZZ BUZZ");
+    }
+
+    public void playGame(String game) {  // activates method to play a game
+      System.out.println("Opening game...");
+    }
+
+    @Override
+    public void call(String number) {
+      super.call(number); // this isn't calling a constructor, so for syntax use ' super.methodName(parameters)''
+      storeCallHistory(number); // rather than just using ' super(parameters); '
+    }
+
+    public MobilePhone(String brand) {
+      super(brand);
+      System.out.println("Is this necessary?");
+    }
+
+    public void storeCallHistory(String currentNumber) { // stores the last 10 numbers dialed
+      if (callHistory == null) {    // if callHistory is empty, it creates a history of 10 numbers
+        callHistory = new String[10];
+        callHistory[9] = currentNumber;
+      } else {
+          for (int i = 9; i >= 0; i--) {  // if the list isn't full, searches for a null space
+            if (callHistory[i] == null) {
+              System.out.println("What does i equal here " + i);
+              callHistory[i] = currentNumber;
+              break;
+            }
+          }
+          if (callHistory[0] != null) {  // if there has been more than 10 calls, knocks out 10th call
+            for (int storedPosition = 9; storedPosition > 0; storedPosition--) { // then moves each stored# down one
+                System.out.println("Stored position is " + storedPosition);
+                String replace = callHistory[storedPosition-1];
+                callHistory[storedPosition] = replace;
+                if (storedPosition == 1 ) {
+                  callHistory[0] = currentNumber;
+                  break;
+                }
+            }
+          }
+        }
+    }
+
+    public void printLastNumbers() {
+      for (int i = 0; i <= 9; i++) {
+        if (this.callHistory[i] == null) {
+          System.out.println("No further numbers stored");
+          break;
+        } else {
+            System.out.println("Stored Call # " + i + ": " + this.callHistory[i]);
+          }
+      }
+    }
+}

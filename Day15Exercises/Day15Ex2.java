8defbad0fd746520ae2b449c7e608d2fbceb9387
Nathan Hanak <nathanhanak@Nathan-Hanaks-MacBook.local>
2016-11-21 15:04:42 +0000

    Adding ex 2. Doesn't compile, see comments at bottom  for solution.

---
+++ Day15Exercises/Day15Ex2.java
@@ -0,0 +1,36 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Day15Ex2 {
+
+  public static void main(String[] args) {
+
+    Day15Ex2 exercise = new Day15Ex2();
+    exercise.launch("Turkey"); // switch this out for user input of 0,2,4,6 to see various results.
+  }
+
+  public void launch(String newElement) {
+
+    List<String> intList = new ArrayList<String>();
+    intList.add("Mashed potatoes");
+    intList.add("Stuffing");
+    intList.add("Cranberry sauce");
+    String bogus = null;
+    try {
+    intList.add(newElement);
+    intList.add(bogus);
+    } catch (Exception ex) {
+        ex.printStackTrace();
+    } catch (NullPointerException ex) {
+        ex.printStackTrace();
+    }
+  }
+}
+
+/**
+*Technically line 24 causes an issue because NullPointerException is an extension of the class Exception. So Exception
+*will catch any problems with null pointers and there will never be a point which this gets to line 24.
+*Program will not compile.
+*( if you remove lines 24-25 this program runs fine, ArrayList allows null Strings to be stored,
+* but this is for demonstration).
+*/

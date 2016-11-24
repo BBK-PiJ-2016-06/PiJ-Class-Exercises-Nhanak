af94c3c2ff6db21d3f434f8d932e074f52d6a2e7
Nathan Hanak <nathanhanak@Nathan-Hanaks-MacBook.local>
2016-11-24 12:45:48 +0000

    cleanup of Day16 files in repo

--- Day16Exercises/Day16Ex4a.java
+++ Day16Exercises/Day16Ex4a.java
@@ -11,27 +11,35 @@ public class Day16Ex4a {
     if (args.length != 2) {
       System.out.println("You entered an invalid amount of files to search");
     } else {
+      BufferedReader in = null;
+      PrintWriter out = null;
         try {
           File file = new File(args[0]);
           if (file.exists()) {
             File file2 = new File(args[1]);
             file2.createNewFile();
-            BufferedReader in = new BufferedReader(new FileReader(file));
-            String line;
-            PrintWriter out = new PrintWriter(file2);
+            in = new BufferedReader(new FileReader(file));
+            String line = null;
+            out = new PrintWriter(file2);
             while ((line = in.readLine()) != null) {
               file2 = out.write(line);
             }
-
           } else {
               System.out.println("File does not exist");
           }
-          } catch (FileNotFoundException ex) {
-              System.out.println("File does not exist");
+        } catch (FileNotFoundException ex) {
+            System.out.println("File does not exist");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+          try{
+            in.close();
+            out.close();
           } catch (IOException ex) {
               ex.printStackTrace();
           }
         }
+      }
 
   }
 }

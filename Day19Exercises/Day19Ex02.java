b3aa8bc65a8a5092f96aa2fdb8537a3b8dc71663
Nathan Hanak <nathanhanak@NathanHsMacBook.home>
2016-12-09 11:40:13 +0000

    Adding Day19 Ex 02

---
+++ Day19Exercises/Day19Ex02.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+
+public class Day19Ex02 {
+
+  public static void main(String[] args){
+    Day19Ex02 exercise = new Day19Ex02();
+    String[] stringArray = {"pizza", "enchilada", "egg", "burgers", "chicken burger", "bacon" };
+    System.out.println( "Original array: "  + Arrays.asList(stringArray));
+
+    Arrays.sort(stringArray, Day19Ex02::eChecker );
+    System.out.println( "Sorted array by Strings containing 'e' : " + Arrays.asList(stringArray) );
+
+  }
+
+  public static int eChecker(String s1, String s2) {
+    int result;
+    if (s1.contains("e") && !s2.contains("e")) {
+      result = -1;
+    } else if (!s1.contains("e") && s2.contains("e")) {
+      result = 1;
+    } else {
+      result = 0;
+    }
+    return result;
+  }
+}
